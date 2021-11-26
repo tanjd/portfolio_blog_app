909981628df65246555bce10446278d0422355a8
tanjd <tanjd@hotmail.com>
2021-11-27 01:19:45 +0800

    Updated STATIC ROOT path

--- portfolio_blog_app/settings.py
+++ portfolio_blog_app/settings.py
@@ -131,7 +131,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
