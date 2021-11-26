92001c2281a2d0e36e1c49c27d2a26e712ea67f8
tanjd <tanjd@hotmail.com>
2021-11-27 00:47:14 +0800

    modified settings.py

--- portfolio_blog_app/settings.py
+++ portfolio_blog_app/settings.py
@@ -131,8 +131,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
