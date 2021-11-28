14571b44db0c140e98067b6f0e08914c86397de7
tanjd <tanjd@hotmail.com>
2021-11-29 02:17:19 +0800

    Uncomment code in settings.py

--- portfolio_blog_app/settings.py
+++ portfolio_blog_app/settings.py
@@ -34,10 +34,18 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
+<<<<<<< Updated upstream
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
+=======
+# print(SECRET_KEY)
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env("DEBUG")
+# print(DEBUG)
+>>>>>>> Stashed changes
 ALLOWED_HOSTS = ["jeddy-portfolio-blog-app.herokuapp.com/", "127.0.0.1"]
 
 
